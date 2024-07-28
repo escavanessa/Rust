bf44b1b846aa14e37479367136f89a9f9f64589e
vanessa <escavanessa@gmail.com>
2024-07-28 17:44:54 -0400

    idk

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,21 @@
 use std::io;
 //io is  library for user input/ output. std stands for standard library
+use rand::Rng;
+//Rng trait defines methods that the random num generator needs and it must be in scope to use.
+
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    //rand::thread_rng gives us the random number were gonna use. 
+    //local to the current thread(?) of execution and is seeding in the OS?
+    //then call the gen range method to gen a random number within the specified range. 
+
+    println!("The secret number is: {secret_number}");
+
     println!("please input your guess");
-    //i dont understand the macro !
+    //i dont understand the macro ! 
 
     let mut guess = String::new(); //mut makes a varibale mutabe - rust vars are immutible on init.
     //guess is being bound to the result of a string::new which is a fn that returns a new instance of a string
